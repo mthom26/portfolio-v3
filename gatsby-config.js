4aa8ddaf0b441c51cfb797ac1e62efc0ee90a2c6
mthom26 <mthom2626@gmail.com>
2019-01-05 14:17:13 +0000

    Adjust some styling

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,7 +10,8 @@ module.exports = {
           {
             resolve: 'gatsby-remark-images',
             options: {
-              maxWidth: 800
+              maxWidth: 800,
+              linkImagesToOriginal: false
             }
           },
           {
