4cfb76f1c5cc53c9ca498368aefdbb36067b93af
mthom26 <mthom2626@gmail.com>
2019-04-11 19:18:59 +0100

    Refactor BlogComponent and BlogPostComponent to functional components

--- src/components/blog/BlogComponent.js
+++ src/components/blog/BlogComponent.js
@@ -1,43 +1,33 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from 'gatsby';
 
 import './Blog.css';
 
-// import chevronLogo from '../../images/chevron-right.svg';
 import { chevronRight } from '../../utils/icons';
 
-class BlogComponent extends Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    const { reference, data } = this.props;
-
-    return (
-      <div ref={reference} id="blog" className="blog">
-        {data.allMarkdownRemark.edges.map(post => (
-          <Link
-            key={post.node.frontmatter.title}
-            to={`/blog/${post.node.frontmatter.slug}`}
-            className="blogPostItem"
-          >
-            <div className="blogPostItemContent">
-              <div className="blogPostItemFrontmatter">
-                <h2>{post.node.frontmatter.title}</h2>
-                <span>{post.node.frontmatter.date}</span>
-              </div>
-              <p>{post.node.excerpt}</p>
+const BlogComponent = ({ reference, data }) => {
+  return (
+    <div ref={reference} id="blog" className="blog">
+      {data.allMarkdownRemark.edges.map(post => (
+        <Link
+          key={post.node.frontmatter.title}
+          to={`/blog/${post.node.frontmatter.slug}`}
+          className="blogPostItem"
+        >
+          <div className="blogPostItemContent">
+            <div className="blogPostItemFrontmatter">
+              <h2>{post.node.frontmatter.title}</h2>
+              <span>{post.node.frontmatter.date}</span>
             </div>
-            <div className="blogPostItemIcon">
-              {/* <img src={chevronLogo} height="48px" /> */}
-              {chevronRight('#e29865')}
-            </div>
-          </Link>
-        ))}
-      </div>
-    )
-  }
-}
+            <p>{post.node.excerpt}</p>
+          </div>
+          <div className="blogPostItemIcon">
+            {chevronRight('#e29865')}
+          </div>
+        </Link>
+      ))}
+    </div>
+  );
+};
 
 export default BlogComponent;
--- src/components/blogPost/BlogPostComponent.js
+++ src/components/blogPost/BlogPostComponent.js
@@ -1,67 +1,60 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from 'gatsby';
 
 import './BlogPost.css';
 
 import { chevronLeft, chevronRight } from '../../utils/icons'; 
 
-class BlogPostComponent extends Component {
-  constructor(props) {
-    super(props);
-  }
+const BlogPostComponent = ({ reference, data })  => {
+  const {
+    frontmatter: { title, date },
+    html
+  } = data.main.edges[0].node;
+  const {
+    title: prevTitle,
+    date: prevDate,
+    slug: prevSlug
+  } = data.prev.edges[0].node.frontmatter;
+  const {
+    title: nextTitle,
+    date: nextDate,
+    slug: nextSlug
+  } = data.next.edges[0].node.frontmatter;
 
-  render() {
-    const { reference } = this.props;
-    const {
-      frontmatter: { title, date },
-      html
-    } = this.props.data.main.edges[0].node;
-    const {
-      title: prevTitle,
-      date: prevDate,
-      slug: prevSlug
-    } = this.props.data.prev.edges[0].node.frontmatter;
-    const {
-      title: nextTitle,
-      date: nextDate,
-      slug: nextSlug
-    } = this.props.data.next.edges[0].node.frontmatter;
-
-    return (
-      <div ref={reference} id="blog" className="blogPostContainer">
-        <div className="blogPost">
-          <div className="blogPostTitle">
-            <h1>{title}</h1>
-            <span>{date}</span>
-            <hr className="blogPostRuler" />
-          </div>
-          <div className="blogPostContent" dangerouslySetInnerHTML={{ __html: html }} />
+  return (
+    <div ref={reference} id="blog" className="blogPostContainer">
+      <div className="blogPost">
+        <div className="blogPostTitle">
+          <h1>{title}</h1>
+          <span>{date}</span>
           <hr className="blogPostRuler" />
-          <div className="blogPostFooter">
-            <Link to={`/blog/${prevSlug}`} className="blogPostPrev">
-              <div className="blogPostPrevIcon">
-                {chevronLeft('#e29865')}
-              </div>
-              <div className="blogPostPrevContent">
-                <h3>{prevTitle}</h3>
-                <span>{prevDate}</span>
-              </div>
-              
-            </Link>
-            <Link to={`/blog/${nextSlug}`} className="blogPostNext">
-              <div className="blogPostNextContent">
-                <h3>{nextTitle}</h3>
-                <span>{nextDate}</span>
-              </div>
-              <div className="blogPostNextIcon">
-                {chevronRight('#e29865')}
-              </div>
-            </Link>
-          </div>
+        </div>
+        <div className="blogPostContent" dangerouslySetInnerHTML={{ __html: html }} />
+        <hr className="blogPostRuler" />
+        <div className="blogPostFooter">
+          <Link to={`/blog/${prevSlug}`} className="blogPostPrev">
+            <div className="blogPostPrevIcon">
+              {chevronLeft('#e29865')}
+            </div>
+            <div className="blogPostPrevContent">
+              <h3>{prevTitle}</h3>
+              <span>{prevDate}</span>
+            </div>
+            
+          </Link>
+          <Link to={`/blog/${nextSlug}`} className="blogPostNext">
+            <div className="blogPostNextContent">
+              <h3>{nextTitle}</h3>
+              <span>{nextDate}</span>
+            </div>
+            <div className="blogPostNextIcon">
+              {chevronRight('#e29865')}
+            </div>
+          </Link>
         </div>
       </div>
-    )
-  }
-}
+    </div>
+  );
+};
 
 export default BlogPostComponent;
